43b8f3f0af226d50fbf94992dc9f5049080b7d56
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2024-06-14 16:16:21 +0300

    added prisma and initialized db

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from 'src/validation/env.schema';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema: envSchema,
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private readonly db: DatabaseService) {}
+  async getHello() {
+    const result = await this.db.post.findMany();
+    console.log(result);
+
     return 'Hello World!';
   }
 }
